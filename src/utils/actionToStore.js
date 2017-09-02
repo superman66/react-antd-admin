aaf9bd9bcd7418b916eb93ae0fda9c0826c79458
Superman <supermanchc@gmail.com>
2017-09-02 23:13:54 +0800

    add actionToStore tool

--- src/utils/actionToStore.js
+++ src/utils/actionToStore.js
@@ -3,7 +3,7 @@ import RequestStatus from '../constants/RequestStatus'
 export function toTable(action) {
   return {
     status: action.status,
-    data: action.status === RequestStatus.SUCCESS ? action.response.items : [],
+    items: action.status === RequestStatus.SUCCESS ? action.response.items : [],
     page: action.status === RequestStatus.SUCCESS ? action.response.page : {}
   }
 }
